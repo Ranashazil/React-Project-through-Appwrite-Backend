7a178e0878725a4c370ca30908191ec92c5ebb59
Rana Shazil <shazilr81@gmail.com>
2024-10-18 18:57:02 +0500

    appwrite backenf  with React project

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+   <img src="https://png.pngtree.com/png-clipart/20210121/ourmid/pngtree-png-transparent-design-letter-s-in-3d-bold-silver-orange-colour-png-image_2781686.jpg" alt="Logo" width={width} />
+  )
+}
+
+export default Logo
